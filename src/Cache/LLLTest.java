dca787ebbba083cfb3c677f37607f45e59d85abc
Vishal Anand <vishalanand23@gmail.com>
2014-04-27 17:26:57 -0700

    llal

--- src/Cache/LLLTest.java
+++ src/Cache/LLLTest.java
@@ -3,8 +3,8 @@ package Cache;
 /**
  * Created by anandv on 27/04/14.
  */
-public class Test {
+public class LLLTest {
     public static void main(String[] args) {
-        
+        System.out.println(2);
     }
 }
