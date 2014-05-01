840e4b7f070e45a4da35bda2d8816ec14de70d32
Vishal Anand <vishalanand23@gmail.com>
2014-05-01 01:50:01 -0700

    basic structure made

--- src/row/Row.java
+++ src/row/Row.java
@@ -1,7 +1,34 @@
 package row;
 
+import column.Column;
+import column.Condition;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anandv on 27/04/14.
  */
 public class Row {
+    private final ArrayList<Column> values = new ArrayList<Column>();
+
+    public List<Column> filter(Iterable<String> columnsName) {
+        List<Column> newColumns = new ArrayList<Column>();
+        for (Column value : values) {
+            for (String columnName : columnsName) {
+                if (value.isNamed(columnName)) {
+                    newColumns.add(value);
+                    break;
+                }
+            }
+        }
+        return newColumns;
+    }
+
+    public boolean apply(Condition condition) {
+        ArrayList<String> columnsName = new ArrayList<String>();
+        columnsName.add(condition.columnName);
+        Column column = filter(columnsName).get(0);
+        return column.apply(condition);
+    }
 }
--- src/table/Table.java
+++ src/table/Table.java
@@ -1,7 +1,60 @@
 package table;
 
+import column.Column;
+import column.Condition;
+import row.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anandv on 27/04/14.
  */
 public class Table {
+    private final List<Row> rows = new ArrayList<Row>();
+
+    private final List<Column> structure;
+
+    public Table(List<Column> structure) {
+        this.structure = structure;
+    }
+
+    public void addRow(Row row) {
+        rows.add(row);
+    }
+
+    public Iterable<Row> query(String[] columnName) {
+        return rows;
+    }
+
+    private List<Row> filter(List<Row> currentRows, Condition condition) {
+        List<Row> newRows = new ArrayList<Row>();
+        for (Row currentRow : currentRows) {
+            if (currentRow.apply(condition)) newRows.add(currentRow);
+        }
+        return newRows;
+    }
+
+    public Column getColumn(String name) {
+        for (Column column : structure) {
+            if (column.isNamed(name)) return column;
+        }
+        return null;
+    }
+
+//    public Iterable<Row> queryOverAnd(String[] columnName, Condition[] conditions) {
+//        List<Row> currentRows = rows;
+//        for (Condition condition : conditions) {
+//            currentRows = filter(currentRows, condition);
+//        }
+//        return currentRows;
+//    }
+//
+//    public Iterable<Row> queryOverOr(Condition[] conditions) {
+//        Set<Row> currentRows = new TreeSet<Row>();
+//        for (Condition condition : conditions) {
+//            currentRows.addAll(filter(rows, condition));
+//        }
+//        return currentRows;
+//    }
 }
